178f32b35f5c8cffd213cba21e9672cf18e9dd0f
SONU <122858100+subhadeepsonu@users.noreply.github.com>
2025-03-19 12:48:24 +0530

    lgin and register

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,7 @@
+import LoginPage from "@/components/forms/loginForm";
+
 export default function Login() {
     return <div className="h-screen w-full flex justify-center items-center">
-        Login
+        <LoginPage />
     </div>
 }
--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,5 +1,7 @@
+import RegisterPage from "@/components/forms/RegisterForm";
+
 export default function Register() {
     return <div className="h-screen w-full flex justify-center items-center">
-        register
+        <RegisterPage />
     </div>
 }
